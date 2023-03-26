3c5b38c1ab2842763ef1dc53f8165637e9b3e2c9
ShakyaCW <108785724+ShakyaCW@users.noreply.github.com>
2023-03-26 14:10:41 +0530

    Task 2,3,4 Completed

---
+++ Task_3/src/Passenger.java
@@ -0,0 +1,48 @@
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Passenger implements Serializable {
+    @Serial
+    private static final long serialVersionUID=1L;
+
+    private String firstName;
+    private String lastName;
+    private String vehicleNo;
+    private double liters;
+
+    public Passenger(String firstName, String lastName, String vehicleNo, double liters) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.vehicleNo = vehicleNo;
+        this.liters = liters;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getVehicleNo() {
+        return vehicleNo;
+    }
+    public double getLiters() {return liters;}
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setVehicleNo(String vehicleNo) {
+        this.vehicleNo = vehicleNo;
+    }
+
+    public void setLiters(int liters) {
+        this.liters = liters;
+    }
+}
